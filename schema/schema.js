f2a8d7a6c7be104e1c88073e316b0acc60fed82d
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-07-30 05:42:12 -1000

    Update schema.js

--- schema/schema.js
+++ schema/schema.js
@@ -2,27 +2,21 @@ const mongoose = require('mongoose');
 
 const socialLinkSchema = new mongoose.Schema({
   platform: { type: String, required: true },
-  url: { type: String, required: true }
+  url: { type: String, required: true },
+});
+
+const otherLinkSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
 });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  bio: { type: String, default: '' }, // Empty string as default value for bio
-  profilePicture: { type: String, default: '' }, // Empty string as default value for profile picture
-  socialLinks: {
-    type: [socialLinkSchema],
-    validate: {
-      validator: function (socialLinks) {
-        return socialLinks.every((link) => link.platform && link.url);
-      },
-      message: 'All social links must have a valid platform and URL.'
-    }
-  },
-  links: [{
-    title: { type: String, required: true },
-    url: { type: String, required: true }
-  }]
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  profilePicture: { type: String },
+  bio: { type: String },
+  socialLinks: [socialLinkSchema],
+  otherLinks: [otherLinkSchema],
 });
 
 const User = mongoose.model('User', userSchema);
