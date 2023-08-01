821e3652f606b55c8a2d1d06fadccb85de996c96
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-08-01 10:44:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 //middlewares
 const app=express();
 
-app.use(cors());
+app.use(cors({
+    origin:'http://localhost:3000',
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
